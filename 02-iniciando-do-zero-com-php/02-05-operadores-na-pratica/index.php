f0eadaa115863355d2d9792dc7cfdf3d71a02d2c
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-04-22 12:24:29 -0300

    Aula 03 - Operadores

--- 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
+++ 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
@@ -8,12 +8,43 @@ echo fullStackPHPClassName("02.05 - Operadores na prática");
  */
 fullStackPHPClassSession("atribuição", __LINE__);
 
+$operadorA = 5;
+
+$operadorA += 5;
+$operadorA -= 5;
+$operadorA *= 5;
+$operadorA /= 5;
+
+var_dump($operadorA);
+
+echo '-------';
+
+$incrementA = 5;
+$incrementB = 5;
+
+var_dump($incrementA++, ++$incrementA, $incrementB--, --$incrementB);
 
 /**
  * [ comparação ] https://php.net/manual/pt_BR/language.operators.comparison.php
  */
 fullStackPHPClassSession("comparação", __LINE__);
 
+$relatedA = 5;
+$relatedB = "5";
+$relatedC = 10;
+
+var_dump(
+    ($relatedA == $relatedB),
+    ($relatedA === $relatedB),
+    ($relatedA != $relatedB),
+    ($relatedA !== $relatedB),
+    ($relatedA > $relatedC),
+    ($relatedA < $relatedC),
+    ($relatedA >= $relatedB),
+    ($relatedA >= $relatedC),
+    ($relatedA < $relatedC),
+    ($relatedA <= $relatedC),
+);
 
 /**
  * [ lógicos ] https://php.net/manual/pt_BR/language.operators.logical.php
@@ -21,7 +52,30 @@ fullStackPHPClassSession("comparação", __LINE__);
 fullStackPHPClassSession("lógicos", __LINE__);
 
 
+$logicA = true;
+$logicB = false;
+
+var_dump(
+    ($logicA && $logicB),
+    ($logicA || $logicB),
+    ($logicA),
+    (!$logicA),
+    (!$logicB),
+    (!$logicB),
+);
+
 /**
  * [ aritiméticos ] https://php.net/manual/pt_BR/language.operators.arithmetic.php
  */
 fullStackPHPClassSession("aritiméticos", __LINE__);
+
+$calcA = 5;
+$calcB = 10;
+
+var_dump(
+    ($calcA + $calcB),
+    ($calcA - $calcB),
+    ($calcA * $calcB),
+    ($calcA / $calcB),
+    ($calcA % $calcB),
+);
