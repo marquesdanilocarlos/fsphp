fd87a62513223e59c841ef0780dba70f7cac029b
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-04 14:37:28 -0300

    Aula 07 - Closures e Generators

--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -7,8 +7,61 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
 
+$myAge = function ($year) {
+    $age = date("Y") - $year;
+    return "Vc tem {$age} anos.";
+};
+
+echo $myAge(1991);
+
+$priceBrl = function ($price) {
+  return "R$ " . number_format($price, 2, ",", ".");
+};
+
+echo "<p>O projeto custa {$priceBrl(3500)}. </p>";
+
+
+$myCart = [];
+
+$myCart["totalPrice"] = 0;
+
+$cartAdd = function($item, $qtd, $price) use (&$myCart) {
+    $myCart[$item] = $qtd * $price;
+    $myCart["totalPrice"] += $myCart[$item];
+};
+
+$cartAdd("HTML5", 1, 497);
+$cartAdd("PHP", 1, 1097);
+
+var_dump($myCart);
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
  */
 fullStackPHPClassSession("generators", __LINE__);
+
+$iterator = 10;
+
+/*$showDates = function($days){
+    for ($day = 0; $day <= $days; $day++) {
+        $saveDates[] = date("d/m/Y", strtotime("+{$day}days"));
+    }
+
+    return $saveDates;
+};
+
+
+foreach ($showDates($iterator) as $date) {
+    echo "<div class='tag'>$date</div><br/><br/>";
+}*/
+
+
+$showDatesGenerator = function($days){
+    for ($day = 0; $day <= $days; $day++) {
+        yield date("d/m/Y", strtotime("+{$day}days"));
+    }
+};
+
+foreach ($showDatesGenerator($iterator) as $date) {
+    echo "<div class='tag'>$date</div><br/><br/>";
+}
