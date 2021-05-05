ae7a76876245adb338d9ffb69c407717fd41d2e8
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-05 13:50:16 -0300

    Aula 13 - Manipulação de objetos

--- 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.04 - Manipulação de objetos");
 
@@ -7,8 +8,46 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+    "name" => "Robson",
+    "company" => "Upinside",
+    "mail" => "cursos@upinside.com.br"
+];
+
+$objProfile = (object)$arrProfile;
+
+var_dump($arrProfile, $objProfile);
+
+echo "<p>{$arrProfile['name']} trabalha na {$arrProfile['company']}</p>";
+echo "<p>{$objProfile->name} trabalha na {$objProfile->company}</p>";
+
+$ceo = $objProfile;
+
+unset($ceo->company);
+
+var_dump($ceo);
+
+$company = new stdClass();
+$company->company = "Upinside";
+$company->ceo = $ceo;
+$company->manager = new stdClass();
+$company->manager->name = "Kaue";
+$company->manager->mail = "kaue@upinside.com.br";
+
+var_dump($company);
+
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+$pdoException = new PDOException();
+
+var_dump([
+    "class" => get_class($pdoException),
+    "methods" => get_class_methods($pdoException),
+    "vars" => get_object_vars($pdoException),
+    "parent" => get_parent_class($pdoException),
+    "subclass" => is_subclass_of($pdoException, "Exception"),
+         ]);
