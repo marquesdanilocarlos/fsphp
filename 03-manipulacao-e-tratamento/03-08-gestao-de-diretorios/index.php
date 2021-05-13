754a65a721d32185f27943eee3d5c5083d967e64
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-13 12:41:33 -0300

    Aula 17 - Manipulação de diretórios

--- 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
+++ 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("03.08 - Gestão de diretórios");
 
@@ -8,13 +9,52 @@ fullStackPHPClassName("03.08 - Gestão de diretórios");
 fullStackPHPClassSession("verificar, criar e abrir", __LINE__);
 
 
+$folder = __DIR__ . "/uploads";
+
+if (!file_exists($folder) && !is_dir($folder)) {
+    mkdir($folder, 0755);
+} else {
+    var_dump(scandir($folder));
+}
+
 /*
  * [ copiar e renomear ] copy | rename
  */
 fullStackPHPClassSession("copiar e renomear", __LINE__);
+$file = __DIR__ . "/newFile.txt";
+
+if (!file_exists($file) && !is_file($file)) {
+    fopen($file, "w");
+} else {
+    copy($file, $folder . "/" . basename($file));
+    //var_dump(filemtime($file), filemtime(__DIR__ . "/uploads/newFile.txt"));
+    rename(
+        __DIR__ . "/uploads/newFile.txt",
+        __DIR__ . "/uploads/newFile-" . time() . "." . pathinfo($file)['extension']
+    );
+}
+
+var_dump(pathinfo($file), scandir($folder), scandir(__DIR__));
 
 
 /*
  * [ remover e deletar ] unlink | rmdir
  */
 fullStackPHPClassSession("remover e deletar", __LINE__);
+
+$dirRemove = __DIR__ . "/remove/";
+$dirFiles = scandir($dirRemove);
+$filesRemove = array_diff($dirFiles, [".", ".."]);
+$filesCount = count($filesRemove);
+
+if ($filesCount >= 1) {
+    echo "<h1>Clearing directory...</h1>";
+    foreach ($dirFiles as $fileItem) {
+        $fileItem = $dirRemove . $fileItem;
+        if (file_exists($fileItem) && is_file($fileItem)) {
+            unlink($fileItem);
+        }
+    }
+} else {
+    rmdir($dirRemove);
+}
