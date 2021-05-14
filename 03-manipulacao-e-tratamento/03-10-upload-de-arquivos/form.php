1a054111039c72a437a76a20805e38c54308250b
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-14 13:50:40 -0300

    Aula 19 - Upload de arquivos

--- 03-manipulacao-e-tratamento/03-10-upload-de-arquivos/form.php
+++ 03-manipulacao-e-tratamento/03-10-upload-de-arquivos/form.php
@@ -1,5 +1,5 @@
-<form name="post" action="./?post=true" method="post" enctype="multipart/form-data">
-    <p style="margin-bottom: 10px; text-align: right"><a href="./" title="Atualizar">Atualizar</a></p>
+<form name="post" action="index.php?post=true" method="post" enctype="multipart/form-data">
+    <p style="margin-bottom: 10px; text-align: right"><a href="index.php" title="Atualizar">Atualizar</a></p>
     <input type="file" name="file"/>
     <button>Enviar Agora!</button>
 </form>
--- 03-manipulacao-e-tratamento/03-10-upload-de-arquivos/index.php
+++ 03-manipulacao-e-tratamento/03-10-upload-de-arquivos/index.php
@@ -7,3 +7,46 @@ fullStackPHPClassName("03.10 - Upload de arquivos");
  * [ upload errors ] http://php.net/manual/pt_BR/features.file-upload.errors.php
  */
 fullStackPHPClassSession("upload", __LINE__);
+
+$folder = __DIR__ . "/uploads";
+
+if (!file_exists($folder) || !is_dir($folder)) {
+    mkdir($folder, 0755);
+}
+
+var_dump(ini_get("upload_max_filesize"), ini_get("post_max_size"));
+var_dump(filetype(__FILE__), mime_content_type(__FILE__));
+
+$getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
+
+if ($_FILES && !empty($_FILES['file']['name'])) {
+    $fileUpload = $_FILES["file"];
+
+    $allowedTypes = [
+        "image/jpg",
+        "image/jpeg",
+        "image/png",
+        "application/pdf"
+    ];
+
+    $newFileName = time() . mb_strstr($fileUpload['name'], ".");
+
+    if (in_array($fileUpload['type'], $allowedTypes)) {
+        if (move_uploaded_file($fileUpload['tmp_name'], $folder . "/" . "$newFileName")) {
+            echo "<p class='trigger success'>Arquivo enviado com sucesso!</p>";
+        } else {
+            echo "<p class='trigger error'>Whoops, erro inesperado!</p>";
+        }
+    } else {
+        echo "<p class='trigger warning'>Whoops, tipo de arquivo não permitido!</p>";
+    }
+
+} elseif(!$_FILES && $getPost) {
+    echo "<p class='trigger warning'>Whoops, parece que o arquivo é muito grande!</p>";
+} elseif ($_FILES) {
+    echo "<p class='trigger warning'>Selecione um arquivo antes de enviar!</p>";
+}
+
+include __DIR__ . "/form.php";
+
+var_dump(scandir($folder));
