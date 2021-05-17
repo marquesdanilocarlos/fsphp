f48aea78a34c9e068d34a251a28dce629b242b89
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-17 13:30:24 -0300

    Aula 24 - Autoload

--- 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
@@ -7,8 +7,35 @@ fullStackPHPClassName("04.04 - Carregamento automático");
  */
 fullStackPHPClassSession("autoload spl psr-4", __LINE__);
 
+/*require __DIR__ . "/source/autoload.php";
+
+use Source\Loading\User;
+use Source\Loading\Address;
+use Source\Loading\Company;
+
+$user = new User();
+$address = new Address();
+$company = new Company();
+
+var_dump($user, $company, $address);*/
+
 
 /*
  * [ autoload composer psr-4 ] https://getcomposer.org/doc/00-intro.md
  */
 fullStackPHPClassSession("autoload composer psr-4", __LINE__);
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Source\Loading\User;
+use Source\Loading\Address;
+use Source\Loading\Company;
+use PHPMailer\PHPMailer\PHPMailer;
+
+$user = new User();
+$address = new Address();
+$company = new Company();
+
+$mailer = new PHPMailer();
+
+var_dump($user, $company, $address, $mailer);
---
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/source/autoload.php
@@ -0,0 +1,22 @@
+<?php
+spl_autoload_register(function(string $class){
+
+    $prefix = "Source\\";
+
+    $baseDir = __DIR__."/";
+
+    $length = strlen($prefix);
+
+    if (strncmp($prefix, $class, $length) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($class, $length);
+
+    $file = $baseDir . str_replace("\\", "/", $relativeClass) . ".php";
+
+    if (file_exists($file)) {
+        require $file;
+    }
+
+});
