d2968ddd1b5f0616d34f6732e5761e28c2821844
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-05-18 15:28:43 -0300

    Aula 26 - Métodos mágicos

--- 04-php-orientado-a-objetos/04-06-interpretacao-e-operacoes-pt2/index.php
+++ 04-php-orientado-a-objetos/04-06-interpretacao-e-operacoes-pt2/index.php
@@ -12,6 +12,15 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("__set", __LINE__);
 
+$fsphp = new \Source\Interpretation\Product();
+$fsphp->handler("Full Stack PHP Developer", 1997);
+
+$fsphp->name = "FSPHP";
+$fsphp->title = "FSPHP";
+$fsphp->value = 197;
+//$fsphp->price = "FSPHP";
+
+var_dump($fsphp);
 
 /*
  * [ get ] Executado automaticamente quando se tenta obter uma propriedade inacessível
@@ -20,6 +29,10 @@ fullStackPHPClassSession("__set", __LINE__);
  */
 fullStackPHPClassSession("__get", __LINE__);
 
+$fsphp->company = "Upinside";
+
+echo "<p>O curso {$fsphp->title} da {$fsphp->company} é o melhor.</p>";
+
 
 /*
  * [ isset ] Executada automaticamente quando um teste ISSET ou EMPTY é executado em uma propriedade inacessível
@@ -27,6 +40,11 @@ fullStackPHPClassSession("__get", __LINE__);
  */
 fullStackPHPClassSession("__isset", __LINE__);
 
+isset($fsphp->phone);
+isset($fsphp->name);
+empty($fsphp->address);
+
+var_dump($fsphp);
 
 /*
  * [ call ] Executada automaticamente quando se tenta usar um método inacessível
@@ -35,6 +53,21 @@ fullStackPHPClassSession("__isset", __LINE__);
  */
 fullStackPHPClassSession("__call", __LINE__);
 
+$fsphp->notFound("Oops", "teste");
+$fsphp->setPrice(1997, 10);
+
+
+
+/*
+ * [ callStatic ] Executada automaticamente quando se tenta usar um método ESTÁTICO inacessível
+ * https://www.php.net/manual/en/language.oop5.overloading.php#object.callstatic
+ *
+ */
+fullStackPHPClassSession("__call", __LINE__);
+
+$fsphp->notFound("Oops", "teste");
+$fsphp->setPrice(1997, 10);
+
 
 /*
  * [ unset ] Executada automaticamente quando se tenta usar unset em uma propriedade inacessível
@@ -42,9 +75,22 @@ fullStackPHPClassSession("__call", __LINE__);
  */
 fullStackPHPClassSession("__toString", __LINE__);
 
+echo $fsphp;
+
 
 /*
  * [ unset ] Executada automaticamente quando se tenta usar unset em uma propriedade inacessível
  * http://php.net/manual/pt_BR/language.oop5.overloading.php#object.unset
  */
 fullStackPHPClassSession("__unset", __LINE__);
+
+unset($fsphp->name, $fsphp->price, $fsphp->data, $fsphp->title);
+
+
+/*
+ * [ invoke ] Executada quando um objeto é executado como se fosse uma função
+ * https://www.php.net/manual/en/language.oop5.magic.php#object.invoke
+ */
+fullStackPHPClassSession("__invoke", __LINE__);
+
+$fsphp(5, "Danilo");
