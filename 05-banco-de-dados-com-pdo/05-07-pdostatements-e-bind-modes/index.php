a4c84b0c4a661e09e3da96846691c4b4d1ba8af5
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-06-07 18:00:54 -0300

    37 - Statent e bind modes

--- 05-banco-de-dados-com-pdo/05-07-pdostatements-e-bind-modes/index.php
+++ 05-banco-de-dados-com-pdo/05-07-pdostatements-e-bind-modes/index.php
@@ -6,11 +6,21 @@ require __DIR__ . "/../source/autoload.php";
 
 use Source\Database\Connect;
 
+$pdo = Connect::getInstance();
+
 /**
  * [ prepare ] http://php.net/manual/pt_BR/pdo.prepare.php
  */
 fullStackPHPClassSession("prepared statement", __LINE__);
 
+$search = filter_input(INPUT_GET, "f");
+
+$stmt = $pdo->prepare("SELECT * FROM users LIMIT 1");
+
+$stmt->execute();
+
+
+var_dump($stmt, $stmt->rowCount(), $stmt->columnCount(), $stmt->fetchAll());
 
 /*
  * [ bind value ] http://php.net/manual/pt_BR/pdostatement.bindvalue.php
@@ -18,11 +28,31 @@ fullStackPHPClassSession("prepared statement", __LINE__);
  */
 fullStackPHPClassSession("stmt bind value", __LINE__);
 
+$stmt = $pdo->prepare("INSERT INTO users (first_name, last_name) VALUES (:first_name, :last_name)");
+
+$stmt->bindValue("first_name", 'Arlindo', PDO::PARAM_STR);
+$stmt->bindValue("last_name", 'Antena', PDO::PARAM_STR);
+
+$stmt->execute();
+
+var_dump($stmt->rowCount(), $pdo->lastInsertId());
 
 /*
  * [ bind param ] http://php.net/manual/pt_BR/pdostatement.bindparam.php
  */
 fullStackPHPClassSession("stmt bind param", __LINE__);
+
+$stmt = $pdo->prepare("INSERT INTO users (first_name, last_name) VALUES (:first_name, :last_name)");
+
+$firstName = "Juvenal";
+$lastName = "Cruz";
+
+$stmt->bindParam("first_name", $firstName, PDO::PARAM_STR);
+$stmt->bindParam("last_name", $lastName, PDO::PARAM_STR);
+
+$stmt->execute();
+
+var_dump($stmt->rowCount(), $pdo->lastInsertId());
 
 
 /*
@@ -30,8 +60,30 @@ fullStackPHPClassSession("stmt bind param", __LINE__);
  */
 fullStackPHPClassSession("stmt execute array", __LINE__);
 
+$stmt = $pdo->prepare("INSERT INTO users (first_name, last_name) VALUES (:first_name, :last_name)");
+
+$user = [
+    "first_name" => "Shaolin",
+    "last_name" => "Matador de porco"
+];
+
+$stmt->execute($user);
+
+var_dump($stmt->rowCount(), $pdo->lastInsertId());
 
 /*
  * [ bind column ] http://php.net/manual/en/pdostatement.bindcolumn.php
  */
 fullStackPHPClassSession("bind column", __LINE__);
+
+$stmt = $pdo->prepare("SELECT * FROM users LIMIT 3");
+
+$stmt->execute();
+
+$stmt->bindColumn("first_name", $name);
+$stmt->bindColumn("email", $email);
+
+while ($user = $stmt->fetch()) {
+    var_dump($user);
+    echo "O e-mail de {$name} é {$email}";
+}
