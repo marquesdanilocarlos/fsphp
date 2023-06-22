f0487db659c16d854a122f7bae943d777c2ef12f
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-06-21 22:34:38 -0300

    Aula 8 - Estruturas de repetição

--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("02.08 - Estruturas de repetição");
 
@@ -7,12 +8,36 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  * [ do while ] https://php.net/manual/pt_BR/control-structures.do.while.php
  */
 fullStackPHPClassSession("while, do while", __LINE__);
+$loop = 1;
+$while = [];
+
+while ($loop <= 20) {
+    $while[] = $loop;
+    $loop++;
+}
+
+var_dump($while);
+
+$loop = 5;
+$while = [];
+
+do {
+    $while[] = $loop;
+    $loop--;
+} while ($loop >= 1);
+
+var_dump($while);
 
 
 /*
+ *
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "<p>$i</p>";
+}
 
 
 /**
@@ -21,8 +46,32 @@ fullStackPHPClassSession("for", __LINE__);
  */
 fullStackPHPClassSession("break, continue", __LINE__);
 
+for ($c = 1; $c <= 10; $c++) {
+    if ($c % 2 === 0) {
+        continue;
+    }
+
+    if ($c > 7) {
+        break;
+    }
+
+    echo "<p>Pulou +2 :: {$c}</p>";
+}
+
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$array = [];
+
+for ($arr = 1; $arr <= 10; $arr++) {
+    $array[] = $arr;
+}
+
+var_dump($array);
+
+foreach ($array as $key => $value) {
+    var_dump("{$key} => {$value}");
+}
