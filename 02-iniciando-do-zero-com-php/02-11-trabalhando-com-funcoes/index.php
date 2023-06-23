7b0215651bf58c88e53940490ddfc7ba6073efc0
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-06-23 11:59:44 -0300

    Aula 11 - Functions

--- 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -6,12 +6,22 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  * [ functions ] https://php.net/manual/pt_BR/language.functions.php
  */
 fullStackPHPClassSession("functions", __LINE__);
+require_once __DIR__."/functions.php";
+
+var_dump(rockBands("Pearl Jam", "AC/DC", "Red Hot Chilli Peppers"));
+var_dump(rockBands("Kiss", "Scorpions", "Queen"));
+
+var_dump(rockBands("Nirvana"));
 
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
+$weight = 60;
+$height = 1.70;
+
+var_dump(imc());
 
 
 /*
@@ -19,8 +29,15 @@ fullStackPHPClassSession("global access", __LINE__);
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = totalPay(200);
+$pay = totalPay(150);
+$pay = totalPay(30);
+var_dump($pay);
+
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump(myTeam("Kaue", "Gustavo", "Gah", "João"));
