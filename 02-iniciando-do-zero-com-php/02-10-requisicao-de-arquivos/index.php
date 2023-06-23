9ff1ee407a850f73b6e1691a5de8550517b197c8
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-06-23 11:16:51 -0300

    Aula 10 - Include e require

--- 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -7,6 +7,22 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  * [ include_once ] https://php.net/manual/pt_BR/function.include-once.php
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
+/*include "file.php";
+echo "Mas o código continua...";*/
+include __DIR__."/header.php";
+
+$profile = new stdClass();
+$profile->name = "Danilo";
+$profile->company = "SECOM";
+$profile->email = "danilo.marques@presidencia.gov.br";
+
+include __DIR__."/profile.php";
+
+$profile = new stdClass();
+$profile->name = "Felipe";
+$profile->company = "SECOM";
+$profile->email = "felipe.jesus@presidencia.gov.br";
+include __DIR__."/profile.php";
 
 
 /*
@@ -14,4 +30,12 @@ fullStackPHPClassSession("include, include_once", __LINE__);
  * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
+/*require "file.php";
+echo "Continuando...";*/
+
+require __DIR__."/config.php";
+echo COURSE;
+require_once __DIR__."/config.php";
+
+var_dump(require_once __DIR__."/config.php");
 
