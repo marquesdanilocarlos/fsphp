ecb3de6dc4c8a255b697c18a2de4e598eeda233d
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-07-13 11:10:31 -0300

    Mais funções para arrays

--- 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
+++ 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
@@ -77,14 +77,41 @@ var_dump(
     $assoc
 );
 
+$notas = [
+    ["aluno" => "Danilo", "nota" => 7],
+    ["aluno" => "Luiz", "nota" => 3],
+    ["aluno" => "João", "nota" => 9],
+    ["aluno" => "Ana", "nota" => 5],
+    ["aluno" => "José", "nota" => 10],
+];
+
+/**
+ * Menor que 0 se o primeiro parâmetro precisar vir antes
+ * Maior que 0 se o segundo parâmetro precisar vir antes
+ */
+usort($notas, function (array $nota1, array $nota2): int {
+    return $nota2["nota"] <=> $nota1["nota"];
+});
+
+var_dump($notas);
+
 /*
  * [ verificação ]  keys | values | in | explode
  */
 fullStackPHPClassSession("verificação", __LINE__);
 
+$novasNotas = [
+    ["aluno" => "Danilo", "nota" => 7],
+    ["aluno" => "Luiz", "nota" => 3],
+    ["aluno" => "Ana", "nota" => 5],
+    ["aluno" => "José", "nota" => 10],
+];
+
 var_dump([
     array_keys($assoc),
-    array_values($assoc)
+    array_values($assoc),
+    "array_key_exists" =>  array_key_exists('band3', $assoc),
+    "array_search" => array_search("AC/DC", $assoc)
 ]);
 
 //Verificar a existência de um valor dentro de um array
@@ -98,6 +125,70 @@ echo "<p>Eu curto {$arrToString} e muitas outras!</p>";
 
 //Transformar uma string em um array
 var_dump(explode(", ", $arrToString));
+
+
+/**
+ * [ Diferenças ]
+ */
+fullStackPHPClassSession("Diferenças de arrays", __LINE__);
+$indexed[] = "Led Zeppelin";
+$assoc["band6"] = "Black Sabbath";
+
+
+var_dump([
+    "array_diff" => array_diff($indexed, $assoc),
+    "array_diff_key" => array_diff_key($indexed, $assoc),
+    "array_diff_assoc" => array_diff_assoc($assoc, $indexed)
+]);
+
+/**
+ * [ União ]
+ */
+fullStackPHPClassSession("União de arrays", __LINE__);
+
+$alunos2021 = [
+    "Vinícius",
+    "Robson",
+    "Ana",
+    'chaveString' => "oioioi"
+];
+
+$novosAlunos = [
+    "Danilo",
+    "Nico",
+    "Brian",
+    'chaveString' => "Que isso porra?"
+];
+
+$alunos2022 = array_merge($alunos2021, $novosAlunos);
+$outrosAlunos = $alunos2021 + $novosAlunos;
+$maisAlgunsAlunos = [...$alunos2021, "Juvenal", ...$novosAlunos];
+
+var_dump($alunos2022, $outrosAlunos, $maisAlgunsAlunos);
+
+/**
+ * [Criação de variáveis]
+ */
+fullStackPHPClassSession("Criação de variáveis através do array", __LINE__);
+$aluno = ["Danilo", 10, 32];
+$banda = ["AC/DC", ["Brian", "Angus"], 1973];
+
+list($nome, $nota, $idade) = $aluno;
+[$banda, $integrantes, $dataFundacao] = $banda;
+
+var_dump([$nome, $nota, $idade, $banda, $integrantes, $dataFundacao]);
+
+$dados = [
+    "nome" => "Danilo",
+    "idade" => 32,
+    "altura" => 1.70
+];
+extract($dados);
+var_dump($nome, $idade, $altura);
+
+$arrayCriado = compact("nome", "nota", "altura", "banda", "integrantes");
+var_dump($arrayCriado);
+
 
 /**
  * [ exemplo prático ] um template view | implode
