ef801fc0a6781a83f408bd24a0c816ff082025f4
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-08-10 13:17:29 -0300

    Aula 31 - Conceitos de abstração

--- 04-php-orientado-a-objetos/04-10-fundamentos-de-abstracao/index.php
+++ 04-php-orientado-a-objetos/04-10-fundamentos-de-abstracao/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("04.10 - Fundamentos da abstração");
 
@@ -9,17 +10,53 @@ require __DIR__ . "/source/autoload.php";
  * mas nunca para ser instanciada por um objeto.
  */
 fullStackPHPClassSession("superclass", __LINE__);
+$client = new \Source\App\User("Danilo", "Marques");
+/*$account = new \Source\Bank\Account(
+    "1600",
+    "21654",
+    $client,
+    1000
+);*/
 
+var_dump($client);
 
 /*
  * [ especialização ] É uma classe filha que implementa a superclasse e se especializa
  * com suas prórpias rotinas
  */
 fullStackPHPClassSession("especialização.a", __LINE__);
+$saving = new \Source\Bank\Saving(
+    "4687",
+    "89435",
+    $client,
+    100
+);
 
+var_dump($saving);
+
+$saving->deposit(1000);
+$saving->extract();
+$saving->withdraw(163);
+$saving->withdraw(1500);
+$saving->extract();
 
 /*
  * [ especialização ] É uma classe filha que implementa a superclass e se especializa
  * com suas prórpias rotinas
  */
 fullStackPHPClassSession("especialização.b", __LINE__);
+$current = new \Source\Bank\Current(
+    "5487",
+"8672",
+$client,
+1000,
+1000);
+
+var_dump($current);
+
+$current->deposit(1000);
+$current->withdraw(2000);
+$current->extract();
+
+$current->withdraw(500);
+$current->extract();
