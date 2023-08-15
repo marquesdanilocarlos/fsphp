0b54e0f1ddc44ea7d9d66ff4c6e9363b36831ceb
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-08-15 19:22:52 -0300

    Aula 43 - Configurações de projeto

---
+++ 06-seguranca-e-boas-praticas/source/Support/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * DATABASE
+ */
+
+const CONF_DB_HOST = "db";
+const CONF_DB_USER = "root";
+const CONF_DB_PASS = "a654321";
+const CONF_DB_NAME = "fsphp";
+
+/**
+ * URLs
+ */
+const CONF_URL_BASE = "http://localhost:8000/fsphp";
+const CONF_URL_ADMIN = CONF_URL_BASE . "/admin";
+const CONF_URL_ERROR = CONF_URL_BASE . "/404";
+
+/**
+ * DATES
+ */
+const CONF_DATE_BR = "d/m/Y H:i:s";
+const CONF_DATE_APP = "Y-m-d H:i:s";
