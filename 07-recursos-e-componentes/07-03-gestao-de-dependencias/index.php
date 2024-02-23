531012ed8aa8e9e8062b535465fd2a40ce01cedc
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-02-22 21:30:40 -0300

    Aula 56 - Utilização do Autoloader do Composer

--- 07-recursos-e-componentes/07-03-gestao-de-dependencias/index.php
+++ 07-recursos-e-componentes/07-03-gestao-de-dependencias/index.php
@@ -1,10 +1,17 @@
 <?php
 
+use Source\Model\User;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("07.03 - Gestão de dependências");
+
+require __DIR__ . "/../vendor/autoload.php";
 
 /*
  * [ get composer ]
  */
 fullStackPHPClassSession("get composer", __LINE__);
 
+$user = (user())->findById(1);
+
+var_dump($user);
