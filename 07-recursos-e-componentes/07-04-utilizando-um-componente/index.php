9d298faf27b2287339ae7534746975851e0ee9a7
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-02-22 22:02:16 -0300

    Aula 57 - Utilização do componente PHPMailer em hard-code

--- 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
+++ 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use PHPMailer\PHPMailer\Exception as MailException;
+use PHPMailer\PHPMailer\PHPMailer;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("07.04 - Utilizando um componente");
 
@@ -10,8 +13,33 @@ require __DIR__ . "/../vendor/autoload.php";
  */
 fullStackPHPClassSession("instance", __LINE__);
 
+$mail = new PHPMailer();
+var_dump($mail);
 
 /*
  * [ configure ]
  */
 fullStackPHPClassSession("configure", __LINE__);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = '172.17.0.1';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = false;                                   //Enable SMTP authentication
+    $mail->Port = 2025;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('marquesdanilocarlos@gmail.com', 'Danilo');
+    $mail->addAddress('danilo.marques@basisti.com.br', 'Danilo Marues');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Email com PHPMailer';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (MailException $e) {
+    echo message()->error($e->getMessage());
+}
