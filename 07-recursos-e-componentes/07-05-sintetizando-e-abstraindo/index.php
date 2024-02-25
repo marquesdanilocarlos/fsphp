421adc85a8c487e79fda72f9512b6e8a42b9cf90
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-02-25 13:17:06 -0300

    Aula 58 - Classe de abstração de envio de emails

--- 07-recursos-e-componentes/07-05-sintetizando-e-abstraindo/index.php
+++ 07-recursos-e-componentes/07-05-sintetizando-e-abstraindo/index.php
@@ -1,5 +1,9 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use Source\Core\Email;
+use Source\Core\Message;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("07.05 - Sintetizando e abstraindo");
 
@@ -9,3 +13,20 @@ require __DIR__ . "/../vendor/autoload.php";
  * [ synthesize ]
  */
 fullStackPHPClassSession("synthesize", __LINE__);
+$phpMailer = new PHPMailer();
+$message = new Message();
+
+$email = (new Email($phpMailer, $message))->bootstrap(
+    'Olá Mundo, esse é meu email!',
+    'Email via rotina da aplicação',
+    'danilo.marques@basis.com.br',
+    'Marques Danilo'
+);
+$email->attach(__DIR__ . '/../../25231.png', 'GitHub');
+$email->attach(__DIR__ . '/../../computer-illustration.png', 'Computer');
+
+if ($email->send()) {
+    echo message()->success('Email enviado com sucesso!');
+} else {
+    echo $email->getMessage();
+}
--- 07-recursos-e-componentes/source/Core/Email.php
+++ 07-recursos-e-componentes/source/Core/Email.php
@@ -2,6 +2,7 @@
 
 namespace Source\Core;
 
+use Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use Source\Interface\EmailInterface;
 
@@ -10,11 +11,25 @@ class Email implements EmailInterface
     private PHPMailer $mail;
     private Message $message;
 
+    private array $data;
+
     public function __construct(PHPMailer $mail, Message $message)
     {
         $this->mail = $mail;
         $this->message = $message;
+        $this->data = [];
         $this->configMail();
+    }
+
+    public function bootstrap(string $subject, string $message, string $toEmail, string $toName): self
+    {
+        $this->data['subject'] = $subject;
+        $this->data['message'] = $message;
+        $this->data['toEmail'] = $toEmail;
+        $this->data['toName'] = $toName;
+        $this->data['attachments'] = [];
+
+        return $this;
     }
 
     public function getMail(): PHPMailer
@@ -25,6 +40,47 @@ class Email implements EmailInterface
     public function getMessage(): Message
     {
         return $this->message;
+    }
+
+    public function send($fromEmail = CONF_MAIL_SENDER_EMAIL, $fromName = CONF_MAIL_SENDER_NAME): bool
+    {
+        if (empty($this->data)) {
+            $this->message->error('Erro ao enviar, favor, verifique os dados.');
+            return false;
+        }
+
+        if (!isEmail($this->data['toEmail'])) {
+            $this->message->error('E-mail de destinatário inválido.');
+            return false;
+        }
+
+        if (!isEmail($fromEmail)) {
+            $this->message->error('E-mail de remetente inválido.');
+            return false;
+        }
+
+        try {
+            $this->mail->Subject = $this->data['subject'];
+            $this->mail->msgHTML($this->data['message']);
+            $this->mail->addAddress($this->data['toEmail'], $this->data['toName']);
+            $this->mail->setFrom($fromEmail, $fromName);
+
+            foreach ($this->data['attachments'] as $filePath => $fileName) {
+                $this->mail->addAttachment($filePath, $fileName);
+            }
+
+            $this->mail->send();
+            return true;
+        } catch (Exception $e) {
+            $this->message->error($e->getMessage());
+            return false;
+        }
+    }
+
+    public function attach(string $filePath, string $fileName): self
+    {
+        $this->data['attachments'][$filePath] = $fileName;
+        return $this;
     }
 
     private function configMail(): void
@@ -38,4 +94,5 @@ class Email implements EmailInterface
         $this->mail->Host = CONF_MAIL_HOST;
         $this->mail->Port = CONF_MAIL_PORT;
     }
+
 }
