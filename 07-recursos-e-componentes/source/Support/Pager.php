b4eeaf21b7b86a24ad0d53a60a4113f3aea8d739
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-03-07 20:45:00 -0300

    Aula 63 - Paginação de resultados

--- 07-recursos-e-componentes/source/Support/Pager.php
+++ 07-recursos-e-componentes/source/Support/Pager.php
@@ -6,7 +6,7 @@ use CoffeeCode\Paginator\Paginator;
 
 class Pager extends Paginator
 {
-    public function __construct(string $link = null, string $title = null, array $first = null, array $last = null)
+    public function __construct(string $link, string $title = null, array $first = null, array $last = null)
     {
         parent::__construct($link, $title, $first, $last);
     }
